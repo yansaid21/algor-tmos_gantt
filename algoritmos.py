f3d7513696491361ad3ebf2460014df2534a8579
yansaid21 <yansaid21@gmail.com>
2022-09-19 21:48:13 -0500

    noche 1

--- algoritmos.py
+++ algoritmos.py
@@ -1,9 +1,13 @@
+import matplotlib.pyplot as plt
+
 print ("ingrese la cantidad de procesos a realizar")
 lista=[]
+TP= 0 #Tiempo desarrollo
+TT = 0 #Tiempo total
 N = int(input("\n >> "))
 for i in range (N) :
     NP= input(f"ingrese nombre proceso {i} \n >>")
-    DP= input(f"ingrese duración proceso {i} \n >>")
+    DP= int(input(f"ingrese duración proceso {i} \n >>"))
     P= input(f"ingrese prioridad del proceso {i} \n >>")
     
     dic={"Nombre_proceso" : NP,
@@ -11,4 +15,5 @@ for i in range (N) :
          "Prioridad_proceso" : P}
     lista.append(dic)
 int(input("ingrese Q \n >>"))
-print(lista)
+print(lista[0]["Nombre_proceso"]) #INGRESAR AL NOMBRE DEL PROCESO EN LOS DICCIONARIOS
+ #tiempo de desarrollo
