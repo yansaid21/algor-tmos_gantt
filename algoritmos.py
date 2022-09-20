39a74195a83fc6762deaa0b81eb0663525c0928c
GeraldineRomer <geraldine.rome0104@gmail.com>
2022-09-20 13:27:46 -0500

    TR y TE funcionan

--- algoritmos.py
+++ algoritmos.py
@@ -1,19 +1,57 @@
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 print ("ingrese la cantidad de procesos a realizar")
 lista=[]
-TP= 0 #Tiempo desarrollo
-TT = 0 #Tiempo total
-N = int(input("\n >> "))
+TE= 0 #Tiempo espera de cada proceso
+TR = 0 #Tiempo respuesta de cada proceso
+lista_prioridad = []
+
+while True:
+    try:
+        N = int(input("\n >> "))
+        break
+    except ValueError:
+        print("     !!! VALOR INVÁLIDO, INGRESE UN NÚMERO !!! ")
+        
 for i in range (N) :
-    NP= input(f"ingrese nombre proceso {i} \n >>")
-    DP= int(input(f"ingrese duración proceso {i} \n >>"))
-    P= input(f"ingrese prioridad del proceso {i} \n >>")
-    
-    dic={"Nombre_proceso" : NP,
-         "Duracion_proceso " : DP,
-         "Prioridad_proceso" : P}
-    lista.append(dic)
-int(input("ingrese Q \n >>"))
+  NP= input(f"ingrese nombre proceso {i} \n >> ")
+  while True:
+    try:
+      DP= int(input(f"ingrese duración proceso {i} \n >> "))
+      break
+    except ValueError:
+      print("     !!! VALOR INVÁLIDO, INGRESE UN NÚMERO !!! ")
+  P = input(f"ingrese prioridad del proceso {i} \n >> ")
+  dic = {"Nombre_proceso" : NP,
+       "Duracion_proceso" : DP,
+       "Prioridad_proceso" : P}
+  lista_prioridad.append(P)
+  lista.append(dic)
+
+while True:
+    try:
+      int(input("ingrese Q \n >> "))
+      break
+    except ValueError:
+      print("     !!! VALOR INVÁLIDO, INGRESE UN NÚMERO !!! ")
+
+print(lista)
 print(lista[0]["Nombre_proceso"]) #INGRESAR AL NOMBRE DEL PROCESO EN LOS DICCIONARIOS
- #tiempo de desarrollo
+ #tiempo total
+suma =0
+total = 0
+for i in range (N):
+  suma += lista[i]["Duracion_proceso"]
+  total += suma
+TR = total/N
+print("TIEMPO DE RESPUESTA")
+print(TR)
+#TE tiempo de desarrollo
+suma = 0
+total = 0
+for i in range (N-1):
+  suma += lista[i]["Duracion_proceso"]
+  total += suma
+TE = total/N
+print("TIEMPO DE ESPERA")
+print(TE)
