4224407d2f5a20a6ffb6b177a31dcde95b63ab85
GeraldineRomer <geraldine.rome0104@gmail.com>
2022-09-20 14:58:16 -0500

    primer intento diagrama gant algoritmo orden de llegada

--- algoritmos.py
+++ algoritmos.py
@@ -1,4 +1,5 @@
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+import numpy as np
 
 print ("ingrese la cantidad de procesos a realizar")
 lista=[]
@@ -8,11 +9,11 @@ lista_prioridad = []
 
 while True:
     try:
-        N = int(input("\n >> "))
+        N = int(input("\n >> ")) #numero de procesos
         break
     except ValueError:
         print("     !!! VALOR INVÁLIDO, INGRESE UN NÚMERO !!! ")
-        
+
 for i in range (N) :
   NP= input(f"ingrese nombre proceso {i} \n >> ")
   while True:
@@ -37,7 +38,8 @@ while True:
 
 print(lista)
 print(lista[0]["Nombre_proceso"]) #INGRESAR AL NOMBRE DEL PROCESO EN LOS DICCIONARIOS
- #tiempo total
+
+#TIEMPOS
 suma =0
 total = 0
 for i in range (N):
@@ -55,3 +57,57 @@ for i in range (N-1):
 TE = total/N
 print("TIEMPO DE ESPERA")
 print(TE)
+
+#primer diagrama de gant
+altura_barras = 10
+tticks = 10
+procesos = []
+rango_horizontal = 0
+for i in range (N):
+  procesos.append(lista[i]["Nombre_proceso"]) #obtener solo el nombre de los procesos
+  rango_horizontal += lista[i]["Duracion_proceso"] #obtener la duración de cada proceso y sumarlo para encontrar hasta donde se extienden las x
+      
+print(procesos)
+
+# Creación de los objetos del plot:
+fig, gantt = plt.subplots()
+
+# Etiquetas de los ejes:
+gantt.set_xlabel("Duración")
+gantt.set_ylabel("Procesos")
+
+# Límites de los ejes:
+gantt.set_xlim(0, rango_horizontal)
+gantt.set_ylim(0, N*altura_barras)
+
+# Divisiones de eje x
+gantt.grid(True, axis="x")
+
+# Divisiones del eje y
+gantt.set_yticks(range(altura_barras, N*altura_barras, altura_barras), minor=True)
+gantt.grid(True, axis='y', which='minor')
+
+# Etiquetas de máquinas:
+gantt.set_yticks(np.arange(altura_barras/2, altura_barras*N - altura_barras/2 + altura_barras, altura_barras))
+gantt.set_yticklabels(procesos)
+
+# "g" = green -> color verde para todos los procesos del algoritmo 1
+# t0 = donde inicia el proceso
+def agregar_tarea(t0, duracion, proceso, nombre):
+  # Índice de la máquina:
+  index_proceso = procesos.index(proceso)
+  print("procesooo")
+  print(procesos[index_proceso])
+  # Posición de la barra:
+  gantt.broken_barh([(t0, duracion)], (altura_barras*index_proceso, altura_barras), facecolors=("g"))
+  # Posición del texto:
+  gantt.text(x=(t0 + duracion/2), y=(altura_barras*index_proceso + altura_barras/2), s=f"{nombre} ({duracion})", va='center', color='white')
+
+agregar_tarea(0, lista[0]["Duracion_proceso"],lista[0]["Nombre_proceso"],lista[0]["Nombre_proceso"])
+suma = 0
+indice = 1
+for indice in range (N):
+  suma += lista[i]["Duracion_proceso"]
+  agregar_tarea(suma, lista[indice]["Duracion_proceso"], lista[indice]["Nombre_proceso"],lista[indice]["Nombre_proceso"])
+  print(lista[indice]["Nombre_proceso"])
+plt.show()
